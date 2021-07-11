b13b88796dacb7a647cf844ead98f1a7233e2078
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-07-11 12:24:52 +0300

    add transactions list based on card

---
+++ src/components/Transactions.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import TransactionItem from './TransactionItem';
+
+const Transactions = (props) => {
+  const { cardId } = props;
+  const [cardTransactions, setCardTransactions] =
+    useState([]);
+  const [cardCurrency, setCardCurrency] = useState('');
+
+  //fetch transactions
+  useEffect(() => {
+    const getTransactions = async (id) => {
+      const url = `data/transactions${id}.json`;
+      console.log(url);
+      const res = await fetch(
+        `data/transactions${id}.json`
+      );
+      const transactions = await res.json();
+      setCardTransactions(transactions.transactions);
+      setCardCurrency(transactions.currency);
+    };
+    getTransactions(cardId);
+    console.log(cardId);
+  }, [cardId, cardCurrency]);
+
+  return (
+    <ul className="card-transactions">
+      {cardTransactions.map((transaction) => (
+        <li
+          className="transaction"
+          key={transaction.id}>
+          <TransactionItem
+            data={transaction}
+            currency={cardCurrency}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Transactions;
